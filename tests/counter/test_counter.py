74f8edbe9353d62a3a1f003e6e79213bc6ae56fc
Leo Araujo <leonardo_02452@hotmail.com>
2022-10-20 16:06:43 -0300

    test: add counter return

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,11 @@
-# from src.counter import count_ocurrences
+import json
+from unittest.mock import mock_open, patch
+from src.counter import count_ocurrences
 
 
 def test_counter():
+    content = {"message": "The night is dark and full of terrors"}
+
+    with patch("builtins.open", mock_open(read_data=json.dumps(content))):
+        assert count_ocurrences("dummy", "T") == 3
     pass
