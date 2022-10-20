ecf36499d4bcf974a4d96d475aa88ec343602b5f
Leo Araujo <leonardo_02452@hotmail.com>
2022-10-20 18:32:52 -0300

    test: read brazilian file function

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,43 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    english_keys_report = [
+        {'title': 'Maquinista', 'salary': '2000', 'type': 'trainee'},
+        {'title': 'Motorista', 'salary': '3000', 'type': 'full time'},
+        {'title': 'Analista de Software',
+            'salary': '4000',
+            'type': 'full time'},
+        {'title': 'Assistente administrativo',
+            'salary': '1700',
+            'type': ' full time'},
+        {'title': 'Auxiliar administrativo',
+            'salary': '1400',
+            'type': ' full time'},
+        {'title': 'Auxiliar usinagem', 'salary': '1400', 'type': ' full time'},
+        {'title': 'Auxiliar de padaria',
+            'salary': '1400',
+            'type': ' full time'},
+        {'title': 'Analista Contábil', 'salary': '1400', 'type': ' full time'},
+        {'title': 'Gerente comercial', 'salary': '5000', 'type': ' full time'},
+        {'title': 'Analista de Departamento Pessoal',
+            'salary': '4000',
+            'type': ' full time'},
+        {'title': 'Esportista de Futebol profissional',
+            'salary': '50000',
+            'type': ' full time'},
+        {'title': 'Analista de crédito',
+            'salary': '4000',
+            'type': ' full time'},
+        {'title': 'Pessoa Programadora',
+            'salary': '3000',
+            'type': ' full time'},
+        {'title': 'Ux Designer',
+            'salary': '3000',
+            'type': ' full time'},
+        {'title': 'Auxiliar de manutenção',
+            'salary': ' 1400', 'type': ' full time'}
+    ]
+
+    translated_report = read_brazilian_file('tests/mocks/brazilians_jobs.csv')
+    assert translated_report == english_keys_report
